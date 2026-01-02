9612251b43aeed42f7b54dc21019df3de3cac0f0
Maybeoff <pgor29@yandex.ru>
2026-01-02 19:40:58 +0300

    Create debian.sh

---
+++ linux/post_install/debian.sh
@@ -0,0 +1,6 @@
+apt update && apt install -y \
+sudo curl wget nano vim git htop \
+ca-certificates gnupg lsb-release \
+apt-transport-https software-properties-common \
+net-tools iproute2 unzip zip \
+build-essential
