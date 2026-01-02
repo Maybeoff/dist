ae44e0da5672704521babda520e33611466fbe47
Maybeoff <pgor29@yandex.ru>
2026-01-02 19:31:52 +0300

    Add script to enable SSH password authentication
    The script backs up the SSH configuration and enables password authentication and root login.

---
+++ linux/ssh_password/script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+SSHD_CFG="/etc/ssh/sshd_config"
+
+backup() {
+  cp "$SSHD_CFG" "$SSHD_CFG.bak.$(date +%F_%H-%M-%S)"
+}
+
+set_cfg() {
+  local key="$1"
+  local val="$2"
+  if grep -qE "^#?$key" "$SSHD_CFG"; then
+    sed -i "s|^#\?$key.*|$key $val|" "$SSHD_CFG"
+  else
+    echo "$key $val" >> "$SSHD_CFG"
+  fi
+}
+
+echo "[*] Backup sshd_config"
+backup
+
+echo "[*] Enabling password auth + root login"
+set_cfg "PasswordAuthentication" "yes"
+set_cfg "PermitRootLogin" "yes"
+set_cfg "PubkeyAuthentication" "yes"
+set_cfg "UsePAM" "yes"
+
+echo "[*] Restarting SSH"
+systemctl restart ssh || systemctl restart sshd
+
+echo "[*] Make sure root has password"
+passwd root
+
+echo "[OK] Done. Password login for root is enabled."
